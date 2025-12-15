4ade22dd5d28a8233b1beb17b23fdd89953c602b
Marcos Amorim <mamorim@outlook.com>
2025-12-15 21:31:48 +0000

    feat(chore): adding a versioning system with bump my version

--- app/main.py
+++ app/main.py
@@ -1,15 +1,17 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
+from app import __version__
 from app.api import redirect as redirect_router
 from app.api import shortener as shortener_router
 
-app = FastAPI(title="URL Shortener Service")
+app = FastAPI(title="URL Shortener Service", version=__version__)
 
 origins = [
     "http://localhost:3000",  # Next dev
     "http://localhost:4200",  # Angular dev
-    "https://angular.rdrt.uk",  # Angular prod (temp)
+    "https://rdrt.uk",  # Angular prod (temp)
+    "https://www.rdrt.uk",  # Angular prod (temp)
     # add your deployed frontend domain here later, e.g.:
     # "https://your-next-app.vercel.app",
 ]
